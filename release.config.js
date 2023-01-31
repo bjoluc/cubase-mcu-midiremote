cf455193b0ba49efb2053f6dcf680886d9921f93
bjoluc <mail@bjoluc.de>
2023-01-31 16:28:22 +0100

    Configure semantic-release and CI workflow

---
+++ release.config.js
@@ -0,0 +1,21 @@
+const config = require("@bjoluc/semantic-release-config-npm");
+
+config.plugins = config.plugins
+  .map((plugin) => {
+    // Remove the NPM plugin
+    if (Array.isArray(plugin) && plugin[0] === "@semantic-release/npm") {
+      return;
+    }
+
+    // Reconfigure the GitHub plugin's `assets` option
+    if (Array.isArray(plugin) && plugin[0] === "@semantic-release/github") {
+      plugin[1].assets = "dist/behringer/xtouch/behringer_xtouch.js";
+    }
+
+    return plugin;
+  })
+  .filter(Boolean);
+
+config.dryRun = true;
+
+module.exports = config;
