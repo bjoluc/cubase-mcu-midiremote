413819b7a8064b12d1ada28d44891edcecfcf0ae
bjoluc <mail@bjoluc.de>
2024-02-23 21:31:15 +0100

    refactor: Simplify `ContextStateVariable` internals

--- src/util/ContextStateVariable.ts
+++ src/util/ContextStateVariable.ts
@@ -9,19 +9,39 @@
  * plain JS variables in the script keep their values while the `MR_ActiveDevice` changes.
  * `ContextStateVariable`s limit the lifetime of their values to the lifetime of the device context
  * (`MR_ActiveDevice`).
+ *
+ * Originally, `ContextStateVariable` was using JSON (de)serialization along with
+ * `context.setState()` and `context.getState()` to store the variable values as strings in the
+ * context itself. This approach would have allowed for multiple active devices of the same script
+ * at the same time – which is not a supported scenario of this script anyway. Hence, the current
+ * implementation uses a minimalistic "reset to default when the device context changes" approach –
+ * presuming that there is only one active script instance at any time.
  */
 export class ContextStateVariable<ValueType> {
-  private static nextVariableId = 0;
-  private name = `contextStateVariable${ContextStateVariable.nextVariableId++}`;
+  private lastContext?: MR_ActiveDevice;
+  private value = this.initialValue;
+
+  private hasContextChanged(context: MR_ActiveDevice) {
+    if (this.lastContext === context) {
+      return false;
+    }
+
+    this.lastContext = context;
+    return true;
+  }
 
   constructor(private initialValue: ValueType) {}
 
   set(context: MR_ActiveDevice, value: ValueType) {
-    context.setState(this.name, JSON.stringify(value));
+    this.hasContextChanged(context);
+    this.value = value;
   }
 
   get(context: MR_ActiveDevice): ValueType {
-    const state = context.getState(this.name);
-    return state === "" ? this.initialValue : JSON.parse(state);
+    if (this.hasContextChanged(context)) {
+      this.value = this.initialValue;
+    }
+
+    return this.value;
   }
 }
